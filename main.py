1463a95b491445483f057d48f210ea66a2b704ff
Daniil Likhachev <d.v.likhachev101@gmail.com>
2020-08-28 13:45:49 +0600

    make code more repeatable

--- main.py
+++ main.py
@@ -3,44 +3,51 @@ from bs4 import BeautifulSoup
 
 f = open("account_info.json", "r")
 login_info = json.loads(f.read())
-
-vk_session = vk_api.VkApi(login=login_info.get("login"), password=login_info.get("password"), token=login_info.get("token"))
-vk_session.auth(token_only=True)
-vk = vk_session.get_api()
-
-unixTime = 1598241600
-oneHour = 3600
-
-url = "http://scpfoundation.net/scp-"
-
+f.close()
 it = 1
-for itera in range(1, 100):
-    try:
-        pageid = random.randint(1, 5000)
-        urlReal = url + str(pageid)
 
-        r = requests.get(urlReal)
-        soup = BeautifulSoup(r.text, 'lxml')
-        
-        titleArt = soup.find("div", id="page-title").string.strip()
+for sessions in range(0, 1000):
+    vk_session = vk_api.VkApi(login=login_info.get("login"), password=login_info.get("password"), token=login_info.get("token"))
+    vk_session.auth(token_only=True)
+    vk = vk_session.get_api()
 
-        pAr = []
-        for p in soup.find("div", id="page-content"):
-            t = str(p)
-            # Get rid of html tags
-            t = re.sub(r'\<[^>]*\>', '', t)
-            if t != '':
-                pAr.append(t)
+    unixTime = 1599224400
+    oneHour = 3600
 
-        for i in range(1,3):
-            pAr.pop(0)
+    url = "http://scpfoundation.net/scp-"
 
-        messageFin = str(titleArt) + '\n' + "Ссылка на статью: " + str(urlReal) + "\n"
+    for itera in range(1, 1000):
+        # try:
+            pageid = random.randint(1, 5000)
+            urlReal = url + str(pageid)
 
-        for i in pAr:
-            messageFin = messageFin + str(i)
+            r = requests.get(urlReal)
+            soup = BeautifulSoup(r.text, 'lxml')
+            
+            titleArt = soup.find("div", id="page-title").string.strip()
 
-        vk.wall.post(owner_id=login_info.get("owner_id"), message=messageFin, publish_date=unixTime+oneHour*it)
-        it = it+1
-    except Exception:
-        print("Exception: " + urlReal)
+            pAr = []
+            for p in soup.find("div", id="page-content"):
+                t = str(p)
+                # Get rid of html tags
+                t = re.sub(r'\<[^>]*\>', '', t)
+                if t != '':
+                    pAr.append(t)
+
+            for i in range(1,3):
+                pAr.pop(0)
+
+            messageFin = str(titleArt) + '\n' + "Ссылка на статью: " + str(urlReal) + "\n"
+
+            for i in pAr:
+                messageFin = messageFin + str(i)
+
+            vk.wall.post(owner_id=login_info.get("owner_id"), message=messageFin, publish_date=unixTime+oneHour*it)
+            print("Posted: " + urlReal)
+            print("Posts number: " + str(it) + "\n")
+            it = it+1
+            if it%60==0:
+                print("relog in session")
+                break
+        # except Exception:
+        #     print("Exception: " + urlReal)
