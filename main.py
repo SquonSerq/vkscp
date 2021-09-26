a74ff241d240031deed95731e5bdc3d2e8065ba6
SquonSerq <squonserq@bk.ru>
2021-09-26 19:19:13 +0600

    remake to bot execution

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-import requests, vk_api, json, random, re
+import requests, vk_api, json, random, re, datetime, time
 from bs4 import BeautifulSoup
+import urllib.request
 
 f = open("account_info.json", "r")
 login_info = json.loads(f.read())
 f.close()
 it = 1
+exceptionCount = 0
+session = 0
 
-for sessions in range(0, 1000):
+unix_time = 1633190400
+oneHour = 3600
+
+while True:
+    session += 1
+    print("Current session: " + str(session))
     vk_session = vk_api.VkApi(login=login_info.get("login"), password=login_info.get("password"), token=login_info.get("token"))
-    vk_session.auth(token_only=True)
+    print(vk_session.auth(token_only=True))
     vk = vk_session.get_api()
 
-    unixTime = 1599224400
-    oneHour = 3600
-
-    url = "http://scpfoundation.net/scp-"
+    post_img = "photo-198135266_457239019"
 
     for itera in range(1, 1000):
-        # try:
-            pageid = random.randint(1, 5000)
-            urlReal = url + str(pageid)
+        try:
+            response = urllib.request.urlopen('https://scpdb.org/random') #https://scpdb.org/random
+        except Exception:
+            print("Caught HTTP Error \n")
+            break
 
-            r = requests.get(urlReal)
+        try:
+            # check if link contains "wikidot" word
+            if response.geturl().find("wiki") == -1:
+                next
+
+            responseScpId = str(response.geturl()).split('/')
+            responseScpId = responseScpId[len(responseScpId)-1]
+
+            url = "https://scpfoundation.net/" + responseScpId
+
+            r = requests.get(url)
             soup = BeautifulSoup(r.text, 'lxml')
             
             titleArt = soup.find("div", id="page-title").string.strip()
@@ -37,17 +54,24 @@ for sessions in range(0, 1000):
             for i in range(1,3):
                 pAr.pop(0)
 
-            messageFin = str(titleArt) + '\n' + "Ссылка на статью: " + str(urlReal) + "\n"
+            messageFin = str(titleArt) + '\n' + "Ссылка на статью: " + url + "\n\n\n"
 
             for i in pAr:
                 messageFin = messageFin + str(i)
-
-            vk.wall.post(owner_id=login_info.get("owner_id"), message=messageFin, publish_date=unixTime+oneHour*it)
-            print("Posted: " + urlReal)
+            unix_time += oneHour
+            vk.wall.post(owner_id=login_info.get("owner_id"), message=messageFin, publish_date=unix_time, attachments=post_img)
+            print("Posted: " + response.geturl())
             print("Posts number: " + str(it) + "\n")
             it = it+1
+            exceptionCount = 0
+            time.sleep(3600)
             if it%60==0:
                 print("relog in session")
                 break
-        # except Exception:
-        #     print("Exception: " + urlReal)
+        except Exception:
+            print("Exception: " + response.geturl())
+            exceptionCount += 1
+            print("Exception number: " + str(exceptionCount))
+            if(exceptionCount == 15):
+                print("Reached max number of exceptions: 15")
+                exit()
